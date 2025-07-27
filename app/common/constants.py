71ca61480d3b8c4558fbb31a175eb00eedcb8ee9
Will Deinzer <willdeinzer@gmail.com>
2025-07-27 19:08:17 -0400

    Set up database, created first endpoints

---
+++ app/common/constants.py
@@ -0,0 +1,9 @@
+from decouple import config
+
+DB_USER = config('DB_USER', '')
+DB_PASSWORD = config('DB_PASSWORD', '')
+DB_HOST = config('DB_HOST', '')
+DB_PORT = config('DB_PORT', default=5432, cast=int)
+DB_NAME = config('DB_NAME', '')
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
--- app/main.py
+++ app/main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy import create_engine, text
+from app.common.constants import DATABASE_URL
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+engine = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global engine
+
+    engine = create_engine(DATABASE_URL, echo=True, pool_size=2, max_overflow=1)
+
+    yield
+
+    if engine:
+        engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
+
+
+def get_db():
+    with engine.connect() as conn:
+        yield conn
 
 @app.get("/")
 def root():
     return {"message": "Hello, World!"}
+
+@app.get("/get_all_books")
+def get_all_books(db=Depends(get_db)):
+    result = db.execute(text("SELECT * FROM books"))
+    return [dict(row._mapping) for row in result.fetchall()]
+
+@app.get("/get_all_reviews")
+def get_all_reviews(db=Depends(get_db)):
+    result = db.execute(text("SELECT * FROM reviews"))
+    return [dict(row._mapping) for row in result.fetchall()]
+

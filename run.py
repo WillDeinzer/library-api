069c193fa26b5dd1f5695817368f91cf74023a92
Will Deinzer <willdeinzer@gmail.com>
2025-08-23 18:26:50 -0400

    Test log

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import uvicorn
 import os
+import logging
 PORT = int(os.getenv("PORT"))
-print(f"Railway PORT={PORT}")
+logging.info(f"Railway PORT={PORT}")
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=PORT)

518b85f22bded2c5098a2326c8666a0ac70b2ccd
Will Deinzer <willdeinzer@gmail.com>
2025-08-23 18:09:50 -0400

    Run file changes

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import uvicorn
 import os
-PORT = int(os.getenv("PORT", 8000))
+PORT = int(os.getenv("PORT"))
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=PORT, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=PORT)
